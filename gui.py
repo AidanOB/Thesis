57148ad149e7e19717fbde930dd11ec789627a43
Aidan O'Brien <aidan.obrien@student.unsw.edu.au>
2015-02-09 23:25:44 +1100

    Updated the options for the gui, partway done with possible customer requirements. Covering the major usages for CubeSats, involving remote sensing, star gazing, communications, etc.
    Required is changing the options frame to be inactive once the run button is clicked.
    
    Run button requirements: Set the options to 'disabled', save the options for the current run to a file, run the neural network and then the genetic algorithm. Enable the graphs to display progress.

--- gui.py
+++ gui.py
@@ -4,13 +4,12 @@ __author__ = "Aidan O'Brien"
 This has the full layout of the gui to enable easy control of the system
 """
 from tkinter import *
-import tkinter as tk
 import matplotlib
 matplotlib.use('TkAgg')
 
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2TkAgg
 from matplotlib.figure import Figure
-import matplotlib.animation as animation
+import matplotlib.animation as anima
 from matplotlib import style
 style.use("ggplot")
 
@@ -26,6 +25,7 @@ class ThesisGui:
         self.generations = 5000
         self.population_size = 1000
         self.mutation_rate = 0.3
+        self.option_width = 20
 
         # Creating a main menu
         self.menu = Menu(master)
@@ -45,7 +45,7 @@ class ThesisGui:
         body_frame = Frame(master)
         body_frame.pack(fill=BOTH)
 
-        options_frame = Frame(body_frame)
+        options_frame = Frame(body_frame, width=400)
         options_frame.pack(side=LEFT)
 
         graph_frame = Frame(body_frame)
@@ -57,11 +57,8 @@ class ThesisGui:
         self.exit_photo = PhotoImage(file='./GuiFiles/exit_icon_20px.gif')
         self.save_photo = PhotoImage(file='./GuiFiles/save_icon_20px.gif')
         self.run_button = Button(toolbar, image=self.run_photo, command=self.test_button)
-        self.run_button.grid(row=0, column=0, padx=2, pady=2)
         self.save_button = Button(toolbar, image=self.save_photo, command=self.doNothing)
-        self.save_button.grid(row=0, column=1, padx=2, pady=2)
         self.exit_button = Button(toolbar, image=self.exit_photo, command=master.quit)
-        self.exit_button.grid(row=0, column=2, padx=2, pady=2)
 
         # Items in the options frame
         self.population_label = Label(options_frame, text='Population Size')
@@ -77,17 +74,76 @@ class ThesisGui:
         self.generations_entry.insert(0, self.generations)
 
         self.mutations_label = Label(options_frame, text='Mutation Rate')
-        self.mutations_slide = Scale(options_frame, orient=HORIZONTAL, width=10, command=self.update_mutation_rate)
+        self.mutations_slide = Scale(options_frame, orient=HORIZONTAL, width=10, length=160,
+                                     command=self.update_mutation_rate)
         self.mutations_slide.set(30)
 
-        self.population_label.grid(row=0, column=0, sticky=E)
-        self.population_entry.grid(row=0, column=1)
-        self.generations_label.grid(row=1, column=0, sticky=E)
-        self.generations_entry.grid(row=1, column=1)
-        self.mutations_label.grid(row=2, column=0, sticky=E)
-        self.mutations_slide.grid(row=2, column=1)
+        # NN parameters
+        self.unit_label = Label(options_frame, text='CubeSat Size')
+        self.unit_default = StringVar(master)
+        self.unit_default.set("2U")
+        self.unit_option = OptionMenu(options_frame, self.unit_default, "1U", "1.5U", "2U", "3U", "4U", "5U", "6U")
+        self.unit_option.configure(width=self.option_width)
+
+        self.size_label = Label(options_frame, text='Size Importance')
+        self.size_default = StringVar(master)
+        self.size_default.set("Average")
+        self.size_option = OptionMenu(options_frame, self.size_default, "Very Unimportant", "Unimportant",
+                                      "Less Than Average", "Average", "More Than Average", "Important",
+                                      "Very Important")
+        self.size_option.configure(width=self.option_width)
+
+        self.mass_label = Label(options_frame, text='Mass Importance')
+        self.mass_default = StringVar(master)
+        self.mass_default.set('Average')
+        self.mass_option = OptionMenu(options_frame, self.mass_default, "Very Unimportant", "Unimportant",
+                                      "Less Than Average", "Average", "More Than Average", "Important",
+                                      "Very Important")
+        self.mass_option.configure(width=self.option_width)
+
+        self.down_label = Label(options_frame, text='Downlink Speed')
+        self.down_default = StringVar(master)
+        self.down_default.set("Average")
+        self.down_option = OptionMenu(options_frame, self.down_default, "Very Slow", "Slow", "Average", "Fast",
+                                      "Very Fast")
+        self.down_option.configure(width=self.option_width)
+
+        self.up_label = Label(options_frame, text='Uplink Speed')
+        self.up_default = StringVar(master)
+        self.up_default.set("Average")
+        self.up_option = OptionMenu(options_frame, self.up_default, "Very Slow", "Slow", "Average", "Fast", "Very Fast")
+        self.up_option.configure(width=self.option_width)
+
+        self.attitude_label = Label(options_frame, text="Attitude Control")
+        self.attitude_default = StringVar(master)
+        self.attitude_default.set("Average")
+        self.attitude_option = OptionMenu(options_frame, self.attitude_default, "Extremely Lenient", "Very Lenient",
+                                          "Lenient", "Average", "Precise", "Very Precise", "Extremely Precise")
+        self.attitude_option.configure(width=self.option_width)
+
+        self.altitude_label = Label(options_frame, text="Altitude Required")
+        self.altitude_default = StringVar(master)
+        self.altitude_default.set("LEO")
+        self.altitude_option = OptionMenu(options_frame, self.altitude_default, "LEO", "Sun-Sync", "Semi-Sync",
+                                          "Geo-Sync")
+        self.altitude_option.configure(width=self.option_width)
+
+        self.remote_sense_label = Label(options_frame, text="Remote Sensing")
+        self.remote_default = StringVar(master)
+        self.remote_default.set("If Possible")
+        self.remote_option = OptionMenu(options_frame, self.remote_default, "No", "If Possible", "Yes")
+        self.remote_option.configure(width=self.option_width)
+
+        self.sense_label = Label(options_frame, text="RS Wavelengths")
+        self.sense_default = StringVar(master)
+        self.sense_default.set("Visual")
+        self.sense_option = OptionMenu(options_frame, self.sense_default, "Ultraviolet", "Blue", "Green", "Red",
+                                       "Visual", "Visual + Near IR", "Near Infrared", "Infrared", "Far Infrared",
+                                       "Thermal Infrared", "Radar")
+        self.sense_option.configure(width=self.option_width)
 
         # Create the Graphs to be plotted
+        # Currently just placeholders for appearance
         self.fig = Figure(figsize=(5, 5), dpi=100)
         self.fig.set_facecolor('white')
         self.sub_plot_one = self.fig.add_subplot(211)
@@ -101,7 +157,52 @@ class ThesisGui:
         # Creating the status bar
         self.status_message = StringVar()
         self.status_bar = Label(master, textvariable=self.status_message, bd=1, relief=SUNKEN, anchor=W)
+
+        self.set_positions()
+
+    def set_positions(self):
+        """
+        A method to set the positions of all the widgets defined in the constructor
+        """
+        # Options frame
+        self.set_options_positions()
+
+        # Toolbar Frame
+        self.set_toolbar_positions()
+
+        # Status bar
         self.status_bar.pack(side=BOTTOM, fill=X)
+
+    def set_options_positions(self):
+        self.population_label.grid(row=0, column=0, sticky=E)
+        self.population_entry.grid(row=0, column=1)
+        self.generations_label.grid(row=1, column=0, sticky=E)
+        self.generations_entry.grid(row=1, column=1)
+        self.mutations_label.grid(row=2, column=0, sticky=E)
+        self.mutations_slide.grid(row=2, column=1)
+        self.unit_label.grid(row=3, column=0, sticky=E)
+        self.unit_option.grid(row=3, column=1)
+        self.mass_label.grid(row=4, column=0, sticky=E)
+        self.mass_option.grid(row=4, column=1)
+        self.size_label.grid(row=5, column=0, sticky=E)
+        self.size_option.grid(row=5, column=1)
+        self.down_label.grid(row=6, column=0, sticky=E)
+        self.down_option.grid(row=6, column=1)
+        self.up_label.grid(row=7, column=0, sticky=E)
+        self.up_option.grid(row=7, column=1)
+        self.altitude_label.grid(row=8, column=0, sticky=E)
+        self.altitude_option.grid(row=8, column=1)
+        self.attitude_label.grid(row=9, column=0, sticky=E)
+        self.attitude_option.grid(row=9, column=1)
+        self.remote_sense_label.grid(row=10, column=0, sticky=E)
+        self.remote_option.grid(row=10, column=1)
+        self.sense_label.grid(row=11, column=0, sticky=E)
+        self.sense_option.grid(row=11, column=1)
+
+    def set_toolbar_positions(self):
+        self.run_button.grid(row=0, column=0, padx=2, pady=2)
+        self.save_button.grid(row=0, column=1, padx=2, pady=2)
+        self.exit_button.grid(row=0, column=2, padx=2, pady=2)
 
     def update_generations(self):
         self.generations = int(self.generations_entry.get())
@@ -122,7 +223,7 @@ class ThesisGui:
 
     def doNothing(self):
         """
-        An empty function to literally do nothing
+        An empty function to literally do nothing. Used for testing and placeholders
         :return:
         """
         pass
